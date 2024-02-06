a83641b5ae68fc9fd2405fe6309e8500f0e025c2
kr1swang <kris19960712@gmail.com>
2024-02-07 00:52:08 +0800

    feat: add more animate

--- src/components/WinningRate.tsx
+++ src/components/WinningRate.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import { animate } from 'framer-motion'
 import { useTranslation } from 'react-i18next'
 
 import { type History } from '@/types/common'
@@ -9,14 +10,23 @@ interface WinningRateProps {
 
 export default function WinningRate({ history }: WinningRateProps) {
   const { t } = useTranslation()
+  const [counter, setCounter] = useState<number>(0)
   const { winning, total, rate } = useMemo(() => {
     const winning = history.filter(({ isWinning }) => isWinning).length
     const total = history.length
-    const rate = total === 0 ? NaN : winning / total
+    const rate = total === 0 ? 0 : winning / total
     return { winning, total, rate }
   }, [history])
   const titleDesc = useMemo(() => (total === 0 ? '' : `(${winning}/${total})`), [winning, total])
-  const rateLabel = useMemo(() => (isNaN(rate) ? '-' : `${Math.round(rate * 1000) / 10}%`), [rate])
+  const rateLabel = useMemo(() => `${Math.round(counter * 1000) / 10}%`, [counter])
+
+  useEffect(() => {
+    const controls = animate(counter, rate, {
+      duration: 0.1,
+      onUpdate: (value) => setCounter(value)
+    })
+    return () => controls.stop()
+  }, [counter, rate])
 
   return (
     <section className={'grid place-items-center gap-2 rounded-sm border p-6 md:col-span-2'}>
