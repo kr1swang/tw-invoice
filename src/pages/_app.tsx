b7fca37f99ce853b6f5a88f972c8c2fdc332d260
kr1swang <kris19960712@gmail.com>
2024-02-07 00:59:47 +0800

    feat: update description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>{'tw-invoice'}</title>
+        <meta name={'description'} content={'Easily Check Taiwan Invoice Lottery Tool.'} />
         <meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
 
         <style
